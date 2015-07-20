58019e41d9ea7324214ab8b91695a246f6b5b941
ehenderson222 <ehendersonbird@gmail.com>
2015-07-20 03:16:31 -0700

    this is a test

---
+++ EnriqueVertical.rb
@@ -0,0 +1,14 @@
+#1.times do 
+#	puts "E"
+#	puts "N"
+#	puts "R"
+#	puts "I"
+#	puts "Q"
+#	puts "U" 
+#	puts "E"
+
+#end
+
+"Enrique".scan(/[Enrique]/) { |x| puts x }
+#print "jimmy".reverse
+#print "jimmy".length
